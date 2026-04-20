0d5a3f699585fa2728bce52428c20447865dd17b
naveengn45 <naveen456gn@gmail.com>
2026-04-20 12:08:36 +0530

    Add files via upload

---
+++ Kadane's Algorithm.py
@@ -0,0 +1,14 @@
+def max_subarray_sum(nums):
+    
+    max_so_far = nums[0]
+    current_max = nums[0]
+
+    for i in range(1, len(nums)):
+        
+        current_max = max(nums[i], current_max + nums[i])
+        max_so_far = max(max_so_far, current_max)
+        
+    return max_so_far
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(max_subarray_sum(nums))
